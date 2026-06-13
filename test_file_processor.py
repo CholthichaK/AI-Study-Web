cb9f61c973a5a91d028056044a8b97fcd3ba6216
Cholthicha Khanijomdi <140520268+CholthichaK@users.noreply.github.com>
2026-06-13 13:08:02 +0700

    Delete test_file_processor.py

--- test_file_processor.py
+++
@@ -1,8 +0,0 @@
-from app.file_processor import extract_text_from_file
-
-file_path = "sample.txt"
-filename = "sample.txt"
-
-text = extract_text_from_file(file_path, filename)
-
-print(text[:1000])
